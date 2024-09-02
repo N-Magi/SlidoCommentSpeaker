8956153255186123536173d58d27f0442aefe4ec
magi <nohira.maki@outlook.jp>
2024-09-02 22:07:51 +0900

    音声生成関数にStream出力を追加

--- VoicevoxRestLib/Class1.cs
+++ VoicevoxRestLib/Class1.cs
@@ -4,23 +4,57 @@ namespace VoicevoxRestLib
 {
 	public class VoicevoxClient
 	{
+		HttpClient client;
+
+		public VoicevoxClient()
+		{
+			HttpClientHandler handler = new HttpClientHandler();
+			handler.UseProxy = false;
+			handler.UseCookies = false;
+			handler.PreAuthenticate = false;
+			handler.AllowAutoRedirect = false;
+
+
+			client = new HttpClient(handler);
+		}
 		public async Task<string> GetQueries(string words)
 		{
-			HttpClient client = new HttpClient();
-			var query = new Uri(@$"http://localhost:50021/audio_query?text={words}&speaker=3");
+			var query = new HttpRequestMessage();
+			query.RequestUri = new Uri(@$"http://127.0.0.1:50021/audio_query?text={words}&speaker=3");
 
-			var result = await client.PostAsync(query, null);
-			return await result.Content.ReadAsStringAsync();
+			query.Headers.Add("Accept", "application/json");
+			query.Method = HttpMethod.Post;
+
+			
+			var result = client.Send(query, CancellationToken.None);
+			var res = await result.Content.ReadAsStringAsync();
+			return res;
 		}
 
-		public async Task Synthesis(string query)
+		public async Task<Stream> Synthesis(string query)
 		{
-			HttpClient client = new HttpClient();
+			//HttpClient client = new HttpClient();
 			StringContent content = new StringContent(query);
 			content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 			//content.Headers.Add("accept", "audio/wav");
 
-			var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:50021/synthesis?speaker=3&enable_interrogative_upspeak=true");
+			var request = new HttpRequestMessage(HttpMethod.Post, "http://127.0.0.1:50021/synthesis?speaker=3&enable_interrogative_upspeak=true");
+			request.Content = content;
+
+			var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+
+
+			return await response.Content.ReadAsStreamAsync();
+		}
+
+		public async Task Synthesis(string query,string filepath)
+		{
+			//HttpClient client = new HttpClient();
+			StringContent content = new StringContent(query);
+			content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+			//content.Headers.Add("accept", "audio/wav");
+
+			var request = new HttpRequestMessage(HttpMethod.Post, "http://127.0.0.1:50021/synthesis?speaker=3&enable_interrogative_upspeak=true");
 			request.Content = content;
 
 			var response = await client.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
